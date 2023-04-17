6e4bac1bd9e07500fd160142fd391c1d06c7856b
ayush.santri <ayush.santri_int@acko.tech>
2023-04-17 23:33:48 +0530

    added view controllers in tab bar

--- Netfilx Clone/MainTabBarViewController.swift
+++ Netfilx Clone/MainTabBarViewController.swift
@@ -12,8 +12,16 @@ class MainTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemYellow
+        
+        let homeViewController = UINavigationController(rootViewController: HomeViewController())
+        let upcomingViewController = UINavigationController(rootViewController: UpcomingViewController())
+        let searchViewController = UINavigationController(rootViewController: SearchViewController())
+        let downloadViewController = UINavigationController(rootViewController: DownloadsViewController())
+        
+        
+        setViewControllers([homeViewController, upcomingViewController, searchViewController, downloadViewController], animated: true)
     }
-
+    
 
 }
 
