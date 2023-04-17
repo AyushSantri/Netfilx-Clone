b1079e8673161730fd09d5c6091859d10ec4c0bd
ayush.santri <ayush.santri_int@acko.tech>
2023-04-18 00:03:27 +0530

    changed the background of view controllers

--- Netfilx Clone/Controllers/HomeViewController.swift
+++ Netfilx Clone/Controllers/HomeViewController.swift
@@ -12,6 +12,6 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
     }
 }
