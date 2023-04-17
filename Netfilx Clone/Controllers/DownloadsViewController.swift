934fc97b6acfef0bb2a036dc400bafa09230293e
ayush.santri <ayush.santri_int@acko.tech>
2023-04-17 23:07:31 +0530

    added another view controller

---
+++ Netfilx Clone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadsViewController.swift
+//  Netfilx Clone
+//
+//  Created by Ayush Santri on 17/04/23.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+}
